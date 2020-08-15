a8aa83144981aa926e847c13cab69e351d7d7ea1
tuan trung <tranvantuantrung@gmail.com>
2020-08-15 11:36:18 +0700

    models

---
+++ api/models/wallet.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const walletSchema = new Schema({
+    walletName: { type: String, required: true },
+    owner: { type: Schema.Types.ObjectId, ref: 'User' },
+    transactions: [
+        {
+        expense: Number,
+        isIncome: Boolean,
+        title: String,
+        description: String
+        }
+    ]
+})
+
+const Wallet = mongoose.model('Wallet', userSchema, 'wallets');
+
+module.exports = Wallet;
+
