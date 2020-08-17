c709669971106bf1405afa9992a1866b44788047
detectiveno2 <nhatthien185@gmail.com>
2020-08-17 20:39:23 +0700

    Update password field

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  email: { type: String, required: true },
-  userName: { type: String, required: true },
-  password: { type: String, required: true },
-  wallets: [
-    {
-      id: { type: Schema.Types.ObjectId, ref: "Wallet" },
-      walletName: { type: String, required: true },
-    },
-  ],
+	email: { type: String, required: true },
+	userName: { type: String, required: true },
+	password: { type: String },
+	wallets: [
+		{
+			id: { type: Schema.Types.ObjectId, ref: 'Wallet' },
+			walletName: { type: String, required: true },
+		},
+	],
 });
 
-const User = mongoose.model("User", userSchema, "users");
+const User = mongoose.model('User', userSchema, 'users');
 
 module.exports = User;
