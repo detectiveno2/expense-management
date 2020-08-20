d61c388d2e41aaf9f4988eb25f053085a92b5fa7
nguynhieu <tthzicc@gmail.com>
2020-08-20 12:13:57 +0700

    Add accountBalance field for wallet model

--- api/models/wallet.model.js
+++ api/models/wallet.model.js
@@ -1,21 +1,22 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
 const walletSchema = new Schema({
-  walletName: { type: String, required: true },
-  owner: { type: Schema.Types.ObjectId, ref: "User" },
-  transactions: [
-    {
-      expense: { type: Number, required: true },
-      isIncome: { type: Boolean, required: true },
-      date: { type: Date, required: true, default: Date.now },
-      title: String,
-      description: String,
-    },
-  ],
+	walletName: { type: String, required: true },
+	owner: { type: Schema.Types.ObjectId, ref: 'User' },
+	transactions: [
+		{
+			expense: { type: Number, required: true },
+			isIncome: { type: Boolean, required: true },
+			date: { type: Date, required: true, default: Date.now },
+			title: String,
+			description: String,
+		},
+	],
+	accountBalance: { type: Number, default: 0 },
 });
 
-const Wallet = mongoose.model("Wallet", userSchema, "wallets");
+const Wallet = mongoose.model('Wallet', userSchema, 'wallets');
 
 module.exports = Wallet;
