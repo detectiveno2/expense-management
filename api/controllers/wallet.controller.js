c627b5abf2a45b0e2441ddc420245ede2bcfb8ba
nguynhieu <tthzicc@gmail.com>
2020-08-22 18:03:37 +0700

    Rename variable

--- api/controllers/wallet.controller.js
+++ api/controllers/wallet.controller.js
@@ -9,9 +9,9 @@ const {
 module.exports.index = async (req, res) => {
 	const { user } = req;
 
-	const ownerOfWaller = await User.find({ email: user.email });
+	const ownerOfWallet = await User.find({ email: user.email });
 
-	const wallet = await Wallet.find({ owner: ownerOfWaller._id });
+	const wallet = await Wallet.find({ owner: ownerOfWallet._id });
 
 	if (!wallet) {
 		res.status(NOT_FOUND_STATUS).send('Wallet not found');
