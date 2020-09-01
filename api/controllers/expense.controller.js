52a6cd34f702f2d1f96f90af9abc0b37bb6bd65c
nguynhieu <tthzicc@gmail.com>
2020-09-01 08:37:43 +0700

    update expense controller

--- api/controllers/expense.controller.js
+++ api/controllers/expense.controller.js
@@ -1,5 +1,29 @@
-module.exports.addExpense = async (req, res) => {
-	// const date = new Date(req.body.date);
+const Wallet = require('../models/wallet.model');
 
-	return res.json('hey');
+module.exports.addExpense = async (req, res) => {
+	const { expense, isIncome, title, description } = req.body;
+	const { _id } = req.user;
+
+	await Wallet.updateOne(
+		{
+			owner: _id,
+		},
+		{
+			$push: {
+				transactions: {
+					expense,
+					isIncome,
+					title,
+					description,
+				},
+			},
+		}
+	);
+
+	const wallet = await Wallet.find({ owner: _id });
+
+	console.log({ _id });
+	console.log({ wallet });
+
+	return res.json({ wallet });
 };
